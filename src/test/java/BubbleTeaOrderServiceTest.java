e6e21f18db114f3429f22c71c0efec732c512a9d
Neelam Sharma <neelam.dixit@gmail.com>
2021-09-29 14:57:11 +0100

    Trying using parameterized test using Enum class

--- src/test/java/BubbleTeaOrderServiceTest.java
+++ src/test/java/BubbleTeaOrderServiceTest.java
@@ -3,6 +3,7 @@ import com.techreturners.bubbleteaordersystem.service.BubbleTeaMessenger;
 import com.techreturners.bubbleteaordersystem.service.BubbleTeaOrderService;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -10,6 +11,10 @@ import testhelper.DummySimpleLogger;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.lang.String;
+import java.util.stream.Collectors;
 
 public class BubbleTeaOrderServiceTest {
 
@@ -28,34 +33,43 @@ public class BubbleTeaOrderServiceTest {
         bubbleTeaOrderService = new BubbleTeaOrderService(dummySimpleLogger, mockMessenger);
     }
 
+
+    @DisplayName("Should pass non-null enum values as method parameters")
     @ParameterizedTest
-    @EnumSource(BubbleTeaTypeEnum.class)
-    @Test
-    public void shouldCreateBubbleTeaOrderRequestWhenCreateOrderRequestIsCalled() {
+    @EnumSource(value = BubbleTeaTypeEnum.class,names = {"OolongMilkTea","JasmineMilkTea","MatchaMilkTea"})
+     public void shouldCreateBubbleTeaOrderRequestWhenCreateOrderRequestIsCalled() {
 
-        //Arrange
-        BubbleTea bubbleTea = new BubbleTea(BubbleTeaTypeEnum.OolongMilkTea, 4.5);
-        BubbleTeaRequest bubbleTeaRequest = new BubbleTeaRequest(paymentDetails, bubbleTea);
+       // System.out.println( Arrays.stream(BubbleTeaTypeEnum.values()).map());
+        BubbleTea bubbleTea = new BubbleTea(Arrays.stream(BubbleTeaTypeEnum.values()).iterator().next(), 4.5);
 
-        BubbleTeaOrderRequest expectedResult = new BubbleTeaOrderRequest(
-                "hello kitty",
-                "sanrio puroland",
-                "0123456789",
-                BubbleTeaTypeEnum.OolongMilkTea
-        );
+            BubbleTeaRequest bubbleTeaRequest = new BubbleTeaRequest(paymentDetails, bubbleTea);
 
-        //Act
-        BubbleTeaOrderRequest result = bubbleTeaOrderService.createOrderRequest(bubbleTeaRequest);
 
-        //Assert
-        assertEquals(expectedResult.getName(), result.getName());
-        assertEquals(expectedResult.getAddress(), result.getAddress());
-        assertEquals(expectedResult.getDebitCardDigits(), result.getDebitCardDigits());
-        assertEquals(expectedResult.getBubbleTeaType(), result.getBubbleTeaType());
+            BubbleTeaOrderRequest expectedResult = new BubbleTeaOrderRequest(
+                    "hello kitty",
+                    "sanrio puroland",
+                    "0123456789",
+                    BubbleTeaTypeEnum.OolongMilkTea
 
-        //Verify Mock was called with the BubbleTeaOrderRequest result object
-        verify(mockMessenger).sendBubbleTeaOrderRequestEmail(result);
-        verify(mockMessenger, times(1)).sendBubbleTeaOrderRequestEmail(result);
+            );
+
+            //Act
+            BubbleTeaOrderRequest result = bubbleTeaOrderService.createOrderRequest(bubbleTeaRequest);
+
+           // Assert
+            assertEquals(expectedResult.getName(), result.getName());
+            assertEquals(expectedResult.getAddress(), result.getAddress());
+            assertEquals(expectedResult.getDebitCardDigits(), result.getDebitCardDigits());
+            System.out.println("expected  " + expectedResult.getBubbleTeaType());
+            System.out.println("Actual  " + result.getBubbleTeaType());
+            assertEquals(expectedResult.getBubbleTeaType(), result.getBubbleTeaType());
+
+            //Verify Mock was called with the BubbleTeaOrderRequest result object
+            verify(mockMessenger).sendBubbleTeaOrderRequestEmail(result);
+            verify(mockMessenger, times(1)).sendBubbleTeaOrderRequestEmail(result);
+
+        }
     }
 
-}
+
+
